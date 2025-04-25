6aecce614cea1d33522d2cf046c9696b2279e8fe
Aman Farkade <afarkade@in.pepper-advantage.com>
2025-04-25 20:15:16 +0530

    first commit4

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,14 @@
-from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.os_manager import ChromeType
 import streamlit as st
-
-options = Options()
-options.add_argument("--disable-gpu")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--headless=new")  # Use 'new' for Chrome >= 109
+import undetected_chromedriver as uc
 
 @st.cache_resource
 def get_driver():
-    return webdriver.Chrome(
-        service=Service(
-            ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-        ),
-        options=options,
-    )
+    options = uc.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    return uc.Chrome(options=options)
 
 driver = get_driver()
 driver.get("https://find-and-update.company-information.service.gov.uk/")
