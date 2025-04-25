a8467ef97aa015750a69d9cc5b7c01bbb2939724
Aman Farkade <afarkade@in.pepper-advantage.com>
2025-04-25 21:54:24 +0530

    first commit8

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,16 +4,19 @@ from selenium.webdriver.chrome.service import Service
 import streamlit as st
 import os
 
-options = Options()
+options = webdriver.ChromeOptions()
+options.binary_location = "/usr/bin/chromium-browser"
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
-@st.cache_resource
-def get_driver():
-    service = Service(executable_path=os.path.join(os.getcwd(), "chromedriver"))
-    return webdriver.Chrome(service=service, options=options)
+driver = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver", options=options)
 
-driver = get_driver()
+# @st.cache_resource
+# def get_driver():
+#     service = Service(executable_path=os.path.join(os.getcwd(), "chromedriver"))
+#     return webdriver.Chrome(service=service, options=options)
+
+# driver = get_driver()
 driver.get("https://find-and-update.company-information.service.gov.uk/")
 st.code(driver.page_source)
